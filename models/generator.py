d2301be42edb421dec73865ae665f51a72e8c507
Hefaj Uddin <hefaj@ecosoftbd.com>
2024-12-02 16:57:51 +0600

    generate text using pretrained model based on retrieve context

---
+++ models/generator.py
@@ -0,0 +1,19 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+def load_generation_model(model_name="gpt2"):
+    """Load a pre-trained text generation model."""
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(model_name)
+    return model, tokenizer
+
+def generate_text(prompt, model, tokenizer, max_length=100):
+    """Generate text based on a prompt."""
+    inputs = tokenizer(prompt, return_tensors="pt")
+    outputs = model.generate(
+        **inputs,
+        max_length=max_length,
+        num_beams=5,
+        early_stopping=True,
+        no_repeat_ngram_size=2
+    )
+    return tokenizer.decode(outputs[0], skip_special_tokens=True)
